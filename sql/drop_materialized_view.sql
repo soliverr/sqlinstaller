5d7ad179e7f61029da9f2dc9aa184b89971bb869
sOliverr <soliverr@gmail.com>
2013-11-21 13:18:11 +0600

    Added script to drop materialized view

---
+++ sql/drop_materialized_view.sql
@@ -0,0 +1,41 @@
+--
+-- Drop materialized view
+--
+-- Usage: drop_materialized_view MVIEW_NAME [SCHEMA]
+--
+
+set verify off
+
+col 1 new_value 1
+col 2 new_value 2
+
+set feedback off
+select NULL "1", NULL "2" from dual where rownum = 0;
+set feedback on
+
+declare
+  l$cnt    integer := 0;
+  l$schema varchar2(64);
+begin
+  -- Current schema
+  if length('&2') > 0 then
+     l$schema := '&2';
+  else
+     select sys_context( 'userenv', 'current_schema' ) into l$schema from dual;
+  end if;
+
+  select count(1) into l$cnt
+    from sys.all_mviews
+   where mview_name = upper('&1')
+     and owner = l$schema;
+
+   if l$cnt > 0 then
+     execute immediate 'drop materialized view ' || l$schema || '.' || '&1';
+     dbms_output.put_line( CHR(10) || 'I: Materialized view ' || l$schema || '.' || upper('&1') || ' droped' );
+   else
+     dbms_output.put_line( CHR(10) || 'W: Materialized view ' || l$schema || '.' || upper('&1') || ' does not exists' );
+   end if;
+end;
+/
+
+undefine 1 2
